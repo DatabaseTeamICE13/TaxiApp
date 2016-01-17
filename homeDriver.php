7311a934cd9c3375c536bfd8b89c199cfb879b23
Dineth Egodage <Dineth Egodage>
2016-01-17 15:41:47 +0530

    homedriver.php was updated and duration and location showing part is done

--- homeDriver.php
+++ homeDriver.php
@@ -5,23 +5,6 @@
 	$driver_id = $_SESSION['userId'];
 	$driver_name = $_SESSION['name'];
 ?>
-
-<script>
-	function myFunction(requestId) {
-		var bid = prompt("Please enter your Bid", "0.00");
-		if (bid != null) {
-			
-			//window.location="MakeBid.php?driverId=<?php echo $driver_id ?>&requestId = 'requestId'&requestId = bid"
-			//insertDriverBid(<?php echo $driver_id ?>,requestId,bid);
-			
-		}
-		//document.write(requestId);
-		
-	}
-
-</script>
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -32,11 +15,137 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
-
+    <script type="text/javascript" src="js/Functions.js"></script>
     <title>Taxi-app Driver Home</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="http://maps.googleapis.com/maps/api/js"></script>
+
+<script type="text/javascript">
+	var map;
+  var markers = [];
+  var gLocation; //geo location of the customer
+  var labels ='Taxi';
+  var startImage = 'Images/start3.png';
+  var endImage = 'Images/end.png';
+  var markerStart;
+  var markerEnd;
+  var directionsDisplay;
+  var directionsService = new google.maps.DirectionsService();
+   function initialize() {
+    directionsDisplay = new google.maps.DirectionsRenderer({
+    polylineOptions: {
+      strokeColor: "purple"
+    }
+  });
+    directionsDisplay.setMap(map);
+    var mapProp = {
+    center:new google.maps.LatLng(document.getElementById("startLat").value, document.getElementById("startLong").value),
+    zoom:12,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+    map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var centerControlDiv = document.createElement('div');
+    centerControlDiv.index = 1;
+    centerControlDiv.style['padding-top'] = '10px';
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
+    var infowindowStart = new google.maps.InfoWindow({
+      content: 'Start'
+    });
+    var infowindowEnd = new google.maps.InfoWindow({
+      content: 'End'
+    });
+    markerStart = new google.maps.Marker({
+      position: new google.maps.LatLng(document.getElementById("startLat").value, document.getElementById("startLong").value),
+      map: map,
+      icon: startImage
+      });
+    markerStart.setVisible(false);
+    infowindowStart.open(map, markerStart);
+    markerEnd = new google.maps.Marker({
+      position: new google.maps.LatLng(document.getElementById("endLat").value, document.getElementById("endLong").value),
+      map: map,
+      icon: endImage
+      });
+    markerEnd.setVisible(false);
+      infowindowEnd.open(map, markerEnd);
+      markers.push(markerStart);
+      markers.push(markerEnd);
+      calcRoute(document.getElementById("startLat").value, document.getElementById("startLong").value,document.getElementById("endLat").value, document.getElementById("endLong").value);
+  }
+  function calcRoute(startLat,startLong,endLat,endLong) {
+        var start = new google.maps.LatLng(startLat, startLong);
+        //var end = new google.maps.LatLng(38.334818, -181.884886);
+        var end = new google.maps.LatLng(endLat, endLong);
+        var bounds = new google.maps.LatLngBounds();
+        bounds.extend(start);
+        bounds.extend(end);
+        map.fitBounds(bounds);
+        var request = {
+            origin: start,
+            destination: end,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+                directionsDisplay.setMap(map);
+                var distance = response.routes[0].legs[0].distance.value;
+                var distanceKm = (distance/1000).toFixed(0);
+                var distanceM = (distance%1000).toFixed(0);
+                var duration = response.routes[0].legs[0].duration.value;
+                var durationHrs = (duration/3600).toFixed(0);
+                var durationMins = (duration%3600/60).toFixed(0);
+                document.getElementById('distance_duration').innerHTML ="Distance: "+distanceKm+"Km "+distanceM+"m. Duration: "+durationHrs+"Hrs "+durationMins+ "mins.";
+            } else {
+                alert("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
+            }
+        });
+
+        
+    }
+    google.maps.LatLng.prototype.kmTo = function(a){ 
+        var e = Math, ra = e.PI/180; 
+        var b = this.lat() * ra, c = a.lat() * ra, d = b - c; 
+        var g = this.lng() * ra - a.lng() * ra; 
+        var f = 2 * e.asin(e.sqrt(e.pow(e.sin(d/2), 2) + e.cos(b) * e.cos 
+        (c) * e.pow(e.sin(g/2), 2))); 
+        return f * 6378.137; 
+    }
+
+    google.maps.Polyline.prototype.inKm = function(n){ 
+        var a = this.getPath(n), len = a.getLength(), dist = 0; 
+        for (var i=0; i < len-1; i++) { 
+           dist += a.getAt(i).kmTo(a.getAt(i+1)); 
+        }
+        return dist; 
+    }
+  //google.maps.event.addDomListener(document.getElementById("basicModal"), 'load', initialize);
+  function myFunction(requestId) {
+		var bid = prompt("Please enter your Bid", "0.00");
+		if (bid != null) {
+			
+			//window.location="MakeBid.php?driverId=<?php echo $driver_id ?>&requestId = 'requestId'&requestId = bid"
+			//insertDriverBid(<?php echo $driver_id ?>,requestId,bid);
+			
+		}
+		//document.write(requestId);
+		
+	}
+  function sendData(startLat,startLong,endLat,endLong){
+    document.getElementById("startLat").value=String(startLat);
+    document.getElementById("startLong").value=String(startLong);
+    document.getElementById("endLat").value=String(endLat);
+    document.getElementById("endLong").value=String(endLong);
+    
+  }
+  
+</script>
+
   </head>
 
   <body>
@@ -108,7 +217,7 @@
 											
 											<td><input type="submit" value="  <?php echo hasBid($driver_id,$request[0])?> " <?php echo enableBid($driver_id,$request[0])?>/></td>
 										</form>
-										
+										<td><a href="#" class="btn btn-sm btn-success" onclick="sendData(<?php echo $request[2]?>,<?php echo $request[1]?>,<?php echo $request[4]?>,<?php echo $request[3]?>);setTimeout(initialize, 500);" data-toggle="modal" data-target="#basicModal">Map</a></td>
 									 </tr>
 									
 						
@@ -120,10 +229,38 @@
                     </div>
                     <br>
                         </div>
-                
-            </div>
+                <!--Modal -->
+                <div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">close x</button>
+                        <h4 class="modal-title" id="myModalLabel">Route for the request</h4>
+                      </div>
+                    <div class="modal-body">
+                    <input type="hidden" id="startLat" name="test" value="initial" />
+                    <input type="hidden" id="startLong" name="test1" value="initial" />
+                    <input type="hidden" id="endLat" name="test2" value="initial" />
+                    <input type="hidden" id="endLong" name="test3" value="initial" />
+                    <pre id="distance_duration"></pre>
+                    
+                    
+                    <div id="googleMap" style="width:500px;height:400px; margin:auto; border: 5px solid #73AD21; padding: 15px;"></div>
+                    </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
         </div>
-		
+
+		 
+
+
+
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
