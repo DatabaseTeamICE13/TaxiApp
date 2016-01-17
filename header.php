af721598a06d60c7b83cb01d76d7817693570149
heshanfernando <heshan92@gmail.com>
2016-01-17 19:24:29 +0530

    wade goda git add *git add *

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', 'On');
     $mysql_host = "localhost";
     $mysql_user = "root";
-    $mysql_pass = "";
+    $mysql_pass = "heshan";
     $mysql_db = "taxiapp";
 
     if(!@mysql_connect($mysql_host,$mysql_user,$mysql_pass) || !@mysql_select_db($mysql_db)){
